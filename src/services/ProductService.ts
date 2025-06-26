51e31698950291e8102e3943e74f1b7af2ac261d
JesusPineda29 <jesuspineda1429@gmail.com>
2025-06-26 16:23:33 -0600

    agregar validar la actualización

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,15 +1,16 @@
-import { safeParse } from "valibot";
+import { safeParse, number, parse, string, transform, pipe } from "valibot";
 import { DraftProductSchema, ProductSchema, ProductsSchema, type Product } from "../types"
 import axios from "axios";
+import { toBoolean } from "../utils";
 
 
- 
+
 type ProductData = {
     [k: string]: FormDataEntryValue;
 }
 
 
-export async function addProduct(data : ProductData) {
+export async function addProduct(data: ProductData) {
     try {
         const result = safeParse(DraftProductSchema, {
             name: data.name,
@@ -17,7 +18,7 @@ export async function addProduct(data : ProductData) {
         });
         if (result.success) {
             const url = `${import.meta.env.VITE_API_URL}/api/products`;
-            await axios.post(url, {name: result.output.name, price: result.output.price, availability: true});
+            await axios.post(url, { name: result.output.name, price: result.output.price, availability: true });
         } else {
             throw new Error('Datos no válidos')
         }
@@ -30,7 +31,7 @@ export async function addProduct(data : ProductData) {
 export async function getProducts() {
     try {
         const url = `${import.meta.env.VITE_API_URL}/api/products`;
-        const {data} = await axios.get(url);
+        const { data } = await axios.get(url);
         const result = safeParse(ProductsSchema, data.data);
         if (result.success) {
             return result.output;
@@ -43,10 +44,10 @@ export async function getProducts() {
 }
 
 
-export async function getProductsById(id:Product['id']) {
+export async function getProductsById(id: Product['id']) {
     try {
         const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
-        const {data} = await axios.get(url);
+        const { data } = await axios.get(url);
         const result = safeParse(ProductSchema, data.data);
         if (result.success) {
             return result.output;
@@ -59,6 +60,17 @@ export async function getProductsById(id:Product['id']) {
 }
 
 
-export async function updateProduct(data : ProductData, id: Product['id']) {
-    console.log(data, id);
+export async function updateProduct(data: ProductData, id: Product['id']) {
+    try {
+        //Este es el schema ya listo
+        const NumberSchema = pipe(string(), transform(Number), number());
+        const result = safeParse(ProductSchema, {
+            id,
+            name: data.name,
+            price: parse(NumberSchema, data.price),
+            availability: toBoolean(data.availability.toString())
+        })
+    } catch (error) {
+        console.log(error);
+    }
 }
